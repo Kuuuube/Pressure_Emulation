f891b2ca9abdb1e232e9eddc790b50bb8c3d06e0
kuuuube <hexagonisalie@gmail.com>
2023-05-08 18:45:56 -0400

    Add continuous mode

--- Pressure_Emulation/Pressure_Emulation_Line.cs
+++ Pressure_Emulation/Pressure_Emulation_Line.cs
@@ -10,12 +10,15 @@ namespace Pressure_Emulation
     public class Pressure_Emulation_Line : IPositionedPipelineElement<IDeviceReport>
     {
         private Vector2 start_position;
+        private Vector2 offset_position;
         private Vector2 output_position;
         private double output_pressure_double;
         private uint last_real_pressure;
+        private uint emulating_pressure_resolution;
+        private bool hold_report = true;
         private uint calc_pressure_change(uint input_pressure) {
-            if (pressure_resolution == 0) {
-                pressure_resolution = 1;
+            if (emulating_pressure_resolution == 0) {
+                emulating_pressure_resolution = 1;
             }
             if (line_length == 0) {
                 line_length = 1;
@@ -24,7 +27,7 @@ namespace Pressure_Emulation
             double pressure_per_px = ((double)max_pressure_resolution / (double)line_length);
             output_pressure_double += pressure_per_px;
 
-            uint pressure_divisor = (max_pressure_resolution / pressure_resolution);
+            uint pressure_divisor = (max_pressure_resolution / emulating_pressure_resolution);
             if (pressure_divisor <= 0) {
                     pressure_divisor = 1;
             }
@@ -37,6 +40,8 @@ namespace Pressure_Emulation
             if (input is ITabletReport tabletReport)
             {
                 if (tabletReport.Pressure <= pressure_deadzone_percent / 100 * max_pressure_resolution) {
+                    hold_report = true;
+                    emulating_pressure_resolution = pressure_resolution;
                     last_real_pressure = 0;
                     output_pressure_double = 0;
                     return input;
@@ -45,13 +50,32 @@ namespace Pressure_Emulation
                 if (tabletReport.Pressure > pressure_deadzone_percent / 100 * max_pressure_resolution && last_real_pressure == 0) {
                     start_position = tabletReport.Position;
                     output_position = tabletReport.Position;
+                    offset_position = start_position;
                     last_real_pressure = tabletReport.Pressure;
                 }
 
-                if (Math.Abs(start_position.X - output_position.X) > line_length + line_offset) {
-                    tabletReport.Pressure = 0;
-                    tabletReport.Position = output_position;
-                    return input;
+                if (Math.Abs(start_position.X - output_position.X) > line_length + line_x_offset) {
+                    if (continuous_mode_pressure_divisor == 0) {
+                            continuous_mode_pressure_divisor = 1;
+                    }
+                    if (!continuous_mode || minimum_pressure_resolution > (uint)(emulating_pressure_resolution / continuous_mode_pressure_divisor)) {
+                        tabletReport.Pressure = 0;
+                        tabletReport.Position = output_position;
+                        return input;
+                    } else {
+                        if (hold_report) {
+                            hold_report = false;
+                            tabletReport.Position = output_position;
+                            tabletReport.Pressure = 0;
+                            return input;
+                        }
+                        hold_report = true;
+                        offset_position = new Vector2(offset_position.X + continuous_mode_x_offset, offset_position.Y + continuous_mode_y_offset);
+                        start_position.X += continuous_mode_x_offset;
+                        output_position = offset_position;
+                        output_pressure_double = 0;
+                        emulating_pressure_resolution = (uint)(emulating_pressure_resolution / continuous_mode_pressure_divisor);
+                    }
                 }
 
                 tabletReport.Pressure = calc_pressure_change(tabletReport.Pressure);
@@ -86,13 +110,33 @@ namespace Pressure_Emulation
             ("Line Length: The length in pixels to draw the line.")]
         public uint line_length { set; get; }
 
-        [Property("Line Offset"), Unit("px"), ToolTip
-            ("Line Offset: The length in pixels to continue drawing after max pressure is reached.")]
-        public uint line_offset { set; get; }
+        [Property("Line X Offset"), Unit("px"), ToolTip
+            ("Line X Offset: The length in pixels to continue drawing after max pressure is reached.")]
+        public uint line_x_offset { set; get; }
 
         [Property("Pressure Deadzone"), Unit("%"), ToolTip
             ("Pressure Deadzone: Adds a pressure deadzone at the set pressure percent (match this value to your Tip Threshold in the Pen Settings tab).")]
         public float pressure_deadzone_percent { set; get; }
+
+        [BooleanProperty("Continuous Mode", ""), ToolTip
+            ("Continuous Mode: Repeats the line drawing after applying the specified offsets and divisors.")]
+        public bool continuous_mode { set; get; }
+
+        [Property("Continuous Mode X Offset"), Unit("px"), ToolTip
+            ("Continuous Mode X Offset: The length in pixels to offset the line in the X axis every repeat.")]
+        public int continuous_mode_x_offset { set; get; }
+
+        [Property("Continuous Mode Y Offset"), Unit("px"), ToolTip
+            ("Continuous Mode Y Offset: The length in pixels to offset the line in the Y axis every repeat.")]
+        public int continuous_mode_y_offset { set; get; }
+
+        [Property("Continuous Mode Pressure Divisor"), ToolTip
+            ("Continuous Mode Pressure Divisor: The number to divide the pressure resolution by every repeat.")]
+        public float continuous_mode_pressure_divisor { set; get; }
+
+        [Property("Minimum Pressure Resolution"), ToolTip
+            ("Minimum Pressure Resolution: The minimum pressure resolution to emulate.")]
+        public uint minimum_pressure_resolution { set; get; }
 
         protected uint max_pressure_resolution;
 
